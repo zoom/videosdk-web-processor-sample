0f5eb63ac7330ebc6d5a584b06ecd2e74b9d4208
Clever Su <34826687+cleversu@users.noreply.github.com>
2025-03-25 18:35:13 +0800

    Update assemblyai-client.ts
    support c-s arch

--- sample/src/utils/assemblyai-client.ts
+++ sample/src/utils/assemblyai-client.ts
@@ -8,32 +8,59 @@ export class AssemblyAIClient {
     this.apiKey = apiKey;
   }
 
+  async getToken() {
+    const response = await fetch("http://localhost:8000/token");
+    const data = await response.json();
+
+    if (data.error) {
+      alert(data.error);
+    }
+
+    return data.token;
+  }
+
   async connect(onMessage: (text: string) => void) {
     try {
-        this.realtimeTranscriber.current = new RealtimeTranscriber({
-            token: this.apiKey,
-            sampleRate: 16_000,
-          });
+      this.realtimeTranscriber = new RealtimeTranscriber({
+        token: await this.getToken(),
+        sampleRate: 16_000,
+      });
+
+      const texts: { [key: number]: string } = {};
+      this.realtimeTranscriber.on("transcript", (message: { audio_start: number; text: string }) => {
+        let msg = "";
+        texts[message.audio_start] = message.text;
+        const keys = Object.keys(texts).map(Number);
+        keys.sort((a, b) => a - b);
+        for (const key of keys) {
+          if (texts[key]) {
+            msg += ` ${texts[key]}`;
+          }
+        }
+        onMessage(msg);
+      });
+      
+      await this.realtimeTranscriber.connect();
     } catch (err) {
-      console.error('AssemblyAI connection error:', err);
+      console.error("AssemblyAI connection error:", err);
       throw err;
     }
   }
 
   sendAudio(audioData: ArrayBuffer) {
-    // if (this.transcriber) {
-    //   const buffer = new Uint8Array(audioData);
-    //   this.transcriber.sendAudio(buffer);
-    // } else {
-    //   console.warn('Attempted to send audio, but transcriber is not connected.');
-    // }
+    if (this.realtimeTranscriber) {
+      const buffer = new Uint8Array(audioData);
+      this.realtimeTranscriber.sendAudio(buffer);
+    } else {
+      console.warn('Attempted to send audio, but transcriber is not connected.');
+    }
   }
 
   async disconnect() {
-    // if (this.transcriber) {
-    //   console.log('Closing real-time transcription connection...');
-    //   await this.transcriber.close();
-    //   this.transcriber = null;
-    // }
+    if (this.realtimeTranscriber) {
+      console.log('Closing real-time transcription connection...');
+      await this.realtimeTranscriber.close();
+      this.realtimeTranscriber = null;
+    }
   }
 }
